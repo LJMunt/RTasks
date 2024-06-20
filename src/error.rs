68fa5d1fc277edea7c7ca83108190e775c93a9af
sogrim <muntwyler.lukas@musys.io>
2024-06-20 15:48:21 +0200

    Implemented custom error for aes

---
+++ src/error.rs
@@ -0,0 +1,51 @@
+use std::fmt;
+use std::io;
+use std::str::Utf8Error;
+use aes_gcm::aead::Error as AesError;
+
+#[derive(Debug)]
+pub enum TaskError {
+    Io(io::Error),
+    Utf8(Utf8Error),
+    Aes(AesError),
+    Hex(hex::FromHexError),
+    Custom(String),
+}
+
+impl fmt::Display for TaskError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            TaskError::Io(err) => write!(f, "IO error: {}", err),
+            TaskError::Utf8(err) => write!(f, "UTF-8 error: {}", err),
+            TaskError::Aes(err) => write!(f, "AES error: {:?}", err),
+            TaskError::Hex(err) => write!(f, "Hex error: {}", err),
+            TaskError::Custom(err) => write!(f, "Error: {}", err),
+        }
+    }
+}
+
+impl std::error::Error for TaskError {}
+
+impl From<io::Error> for TaskError {
+    fn from(err: io::Error) -> TaskError {
+        TaskError::Io(err)
+    }
+}
+
+impl From<Utf8Error> for TaskError {
+    fn from(err: Utf8Error) -> TaskError {
+        TaskError::Utf8(err)
+    }
+}
+
+impl From<AesError> for TaskError {
+    fn from(err: AesError) -> TaskError {
+        TaskError::Aes(err)
+    }
+}
+
+impl From<hex::FromHexError> for TaskError {
+    fn from(err: hex::FromHexError) -> TaskError {
+        TaskError::Hex(err)
+    }
+}
